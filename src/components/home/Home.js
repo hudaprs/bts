8f5148d6d91acfe6798db0bfff81f1dc8767d859
Huda Prasetyo <huda.prs2002@gmail.com>
2020-07-29 16:57:20 +0700

    add expertise

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -83,8 +83,52 @@ const Home = () => {
         </div>
       </section>
 
-      {/* Text Expertise */}
-      <section id='tech-expertise' className='py-3'></section>
+      {/* Tech Expertise */}
+      <section id='tech-expertise' className='py-3'>
+        <div className='container'>
+          <div className='showcase-container showcase-container-each'>
+            <div className='tech-expertise-content'>
+              <div>
+                <p className='m-heading'>Tech Expertise</p>
+                <p className='showcase-text justify mb-4'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore.
+                </p>
+                {/* Tech List */}
+                <div className='tech-list'>
+                  <img
+                    src={require("../../assets/img/TechExpertise/Node.png")}
+                    alt='Node'
+                  />
+                  <img
+                    src={require("../../assets/img/TechExpertise/React.png")}
+                    alt='React'
+                  />
+                  <img
+                    src={require("../../assets/img/TechExpertise/Xamarin.png")}
+                    alt='Xamarin'
+                  />
+                  <img
+                    src={require("../../assets/img/TechExpertise/Angular.png")}
+                    alt='Angular'
+                  />
+                </div>
+              </div>
+              <div>
+                <img
+                  src={require("../../assets/img/TechExpertise/1.png")}
+                  alt='Tech Expertise'
+                  className='text-expertise-img'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Fragment>
   )
 }
