7bdc3de41c71d9066158d48ad2512ef33e4ad06c
Huda Prasetyo <huda.prs2002@gmail.com>
2020-08-03 13:41:39 +0700

    add navigation to navbar and adding dropdown in navbar

--- src/components/inc/home/Navbar.js
+++ src/components/inc/home/Navbar.js
@@ -1,9 +1,43 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
+  const location = window.location.href.split("/")[3]
+  const [scrolled, setScrolled] = useState(false)
+  const [navTop, setNavTop] = useState(true)
+  const [translateY, setTranslateY] = useState("0px")
+
+  const onScroll = () => {
+    if (window.pageYOffset > 100) {
+      setNavTop(false)
+      if (!scrolled) {
+        setTranslateY("-70px")
+      }
+      setTimeout(() => {
+        setTranslateY("0")
+        setScrolled(true)
+      }, 200)
+    } else {
+      setNavTop(true)
+      setScrolled(false)
+    }
+  }
+
+  // Navbar animation
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll)
+
+    return () => window.removeEventListener("scroll", onScroll)
+  }, [scrolled, navTop, translateY])
+
   return (
-    <div id='navbar'>
+    <div
+      id='navbar'
+      className={`${navTop ? "top" : ""}`}
+      style={{
+        transform: `translateY(${translateY})`
+      }}
+    >
       <div className='container'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo'>
@@ -15,10 +49,10 @@ const Navbar = () => {
           </Link>
 
           <ul className='nav-item nav-item-responsive'>
-            <li>
+            <li className={location === "services" ? "current" : ""}>
               <Link to='/services'>Services</Link>
             </li>
-            <li>
+            <li className={location === "industries" ? "current" : ""}>
               <Link to='/industries'>Industries</Link>
             </li>
             <li>
@@ -27,8 +61,19 @@ const Navbar = () => {
             <li>
               <a href='#!'>Blog</a>
             </li>
-            <li>
-              <a href='#!'>Company</a>
+            <li className='nav-dropdown'>
+              <a href='#!'>
+                Company
+                <em
+                  className='fas fa-caret-down'
+                  style={{ marginLeft: "5px" }}
+                ></em>
+              </a>
+              <div className='nav-dropdown-content'>
+                <a href='#!'>Link 1</a>
+                <a href='#!'>Link 2</a>
+                <a href='#!'>Link 3</a>
+              </div>
             </li>
             <li>
               <a href='#!'>
@@ -45,5 +90,4 @@ const Navbar = () => {
     </div>
   )
 }
-
 export default Navbar
--- src/components/industry/WhatWeDo.js
+++ src/components/industry/WhatWeDo.js
@@ -1,57 +1,58 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const WhatWeDo = () => {
-	return (
-		<section id='what-we-do' className='py-4'>
-			<div className='container'>
-				<h3 className='m-heading text-left mb-4'>
-					What <strong>We Do</strong>
-				</h3>
+  return (
+    <section id='what-we-do' className='py-4'>
+      <div className='container'>
+        <h3 className='m-heading text-left mb-4'>
+          What <strong>We Do</strong>
+        </h3>
 
-				<ul className='menu-list mb-4'>
-					<li className='active'>
-						<Link to='#!'>Manufacturing</Link>
-					</li>
-					<li className=''>
-						<Link to='#!'>E-commerce</Link>
-					</li>
-					<li className=''>
-						<Link to='#!'>Financial</Link>
-					</li>
-					<li className=''>
-						<Link to='#!'>Education</Link>
-					</li>
-					<li className=''>
-						<Link to='#!'>Logistic</Link>
-					</li>
-				</ul>
+        <ul className='menu-list mb-4'>
+          <li className='current'>
+            <Link to='#!'>Manufacturing</Link>
+          </li>
+          <li className=''>
+            <Link to='#!'>E-commerce</Link>
+          </li>
+          <li className=''>
+            <Link to='#!'>Financial</Link>
+          </li>
+          <li className=''>
+            <Link to='#!'>Education</Link>
+          </li>
+          <li className=''>
+            <Link to='#!'>Logistic</Link>
+          </li>
+        </ul>
 
-				<div className='grid-col-2'>
-					<div>
-						<img
-							src={require("../../assets/img/Industry/WhatWeDo.png")}
-							alt='Tech Expertise'
-							className='common-img'
-						/>
-					</div>
-					<div>
-						<h3 className='m-heading mb-4' style={{width: "100%"}}>
-							About <strong>Manufacturing</strong>
-						</h3>
-						<p className='lead text-justify mb-2' style={{fontSize: '18px'}}>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua.
-							Ut enim ad minim veniam, quis nostrud exercitation ullamco
-							laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-							dolor in reprehenderit in voluptate velit esse cillum dolore. quis nostrud exercitation ullamco laboris
-							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.
-						</p>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+        <div className='grid-col-2'>
+          <div>
+            <img
+              src={require("../../assets/img/Industry/WhatWeDo.png")}
+              alt='Tech Expertise'
+              className='common-img'
+            />
+          </div>
+          <div>
+            <h3 className='m-heading mb-4' style={{ width: "100%" }}>
+              About <strong>Manufacturing</strong>
+            </h3>
+            <p className='lead text-justify mb-2' style={{ fontSize: "18px" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore. quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default WhatWeDo
